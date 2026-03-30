f873399a83202271a6b0121020d3be4e54695f24
juhyeong <kjhes001@gmail.com>
2026-03-30 14:44:34 +0900

    2차원 배열 실습

---
+++ array/2array-2.c
@@ -0,0 +1,20 @@
+﻿#define _CRT_SECURE_NO_WARNINGS   
+#include <stdio.h>
+//이차원 배열
+int main(void)
+{
+    int i;
+    char menu[5][10] = { //5행0~4(세로) 10열0~9(가로)
+        "init", //1 행 초기값 설정
+        "open", //
+        "close",
+        "read",
+        "write"
+    };
+
+
+    for (i = 0; i < 5; i++)
+        printf("%d 번째 메뉴: %s \n", i, menu[i]); // 인덱스를 다 안쓰고 하나만 쓰면 맨 앞 인덱스만 적용 행만 쓰고 열을 생략함
+
+    return 0;
+}
