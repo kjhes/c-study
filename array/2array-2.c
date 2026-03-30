e0f02e9e733bdf606381f366e6a77b20303d3968
juhyeong <kjhes001@gmail.com>
2026-03-30 15:21:39 +0900

    2차 수정

--- array/2array-2.c
+++ array/2array-2.c
@@ -4,16 +4,16 @@
 int main(void)
 {
     int i;
-    char menu[5][10] = { //5행0~4(세로) 10열0~9(가로)
+    char menu[3][10] = { //5행0~4(세로) 10열0~9(가로)
         "init", //1 행 초기값 설정
         "open", //
         "close",
-        "read",
-        "write"
+        /*"read",
+        "write"*/
     };
 
 
-    for (i = 0; i < 5; i++)
+    for (i = 0; i < 3; i++)
         printf("%d 번째 메뉴: %s \n", i, menu[i]); // 인덱스를 다 안쓰고 하나만 쓰면 맨 앞 인덱스만 적용 행만 쓰고 열을 생략함
 
     return 0;
