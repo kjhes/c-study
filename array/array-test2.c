f6daa0f30df9b602a9e57773655f6281ae8cc762
juhyeong <kjhes001@gmail.com>
2026-03-30 15:46:05 +0900

    문자열 배열 교환 알고리즘1

---
+++ array/array-test2.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#define _CRT_SECURE_NO_WARNINGS
+int main() {
+	char s[] = "ABCD";
+	int a = 0, b = 3;
+	char t;
+	while (a < b)
+	{
+		t = s[a];
+		s[a] = s[b];
+		s[b] = t;
+		a++, b--;
+
+
+
+	}
+
+	printf("%s", s);
+
+
+
+	return 0;
+}
