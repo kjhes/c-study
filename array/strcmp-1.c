2669d57cd77f0af68ab3499783600555a6ac633b
juhyeong <kjhes001@gmail.com>
2026-03-31 11:32:29 +0900

    string.h 실습

---
+++ array/strcmp-1.c
@@ -0,0 +1,25 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h> 
+#include <string.h>
+int main() {
+	int res;
+	char a[30];
+	char b[30];
+	
+	printf("문자열1을 입력하세요");
+	scanf("%s", a); //&a[3]
+	printf("문자열2를 입력하세요");
+	scanf("%s", b);
+	res =strcmp(a, b); //문자열 비교
+	//res 0, 음수 , 양수
+	if (res == 0) //반환값이 0이면 완전히 같은 문자열 이다
+		printf("0입니다");
+	else if (res > 0) // 양수이면 문자열1이 문자열2보다 사전순으로 뒤에 있음
+		printf("%s가 %s보다 뒤",a,b);
+	else if (res < 0) // 음수이면 문자열1이 문자열2보다 사저눈으로 앞에 있음
+		printf("%s가 %s보다 앞",a,b);
+
+	printf("%s , %s ", a, b);
+
+	return 0;
+}
---
+++ array/strcpy-1.c
@@ -0,0 +1,24 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h> //scanf , printf 입출력에 관련된 헤더파일
+#include <string.h> //전처리기 <헤더파일> (프로그램 컴파일 하기 전에 링크 역할을 함)
+//문지열 함수를 사용하기 위해 불러옴
+int main() {
+	char a[10] = "HELLO";// 10바이트 공간에 실질적으로 9개의 글자를 넣을 수 있다 (널이 한칸 차지하기 때문에_)
+	//문자열 배열에 초기값을 한번에 넣어주면 널 값이 알아서 들어간다
+	printf("%c", a[0]);
+	printf("%s", a);
+
+	char b[10]; // 초기값 없이
+	//b[0] = 'L'; // 인덱스로 문자를 넣어 주면 null이 어디에 들어갈 지 b가 알 수 없어서 쓰레기값이 나온다.
+	//b[1] = 'O';
+	//b[2] = 'V';
+	//b[3] = 'E';
+	//b = "love"; //이 방법은 안된다 선언후 한번에 지정 불가능
+	//그래서 필요한게 strcpy()
+	strcpy(b, "love");
+	printf("%s", b);
+
+
+
+	return 0;
+}
